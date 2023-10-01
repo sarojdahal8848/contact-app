88f285cfa4daad4d3f24676a57ae524de123c683
saroj dahal <sarojdahal8848@gmail.com>
2023-10-01 12:30:38 +0545

    build: setup nx with node and next

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello World! from Nx' });
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] Server is running at http://${host}:${port}`);
 });
